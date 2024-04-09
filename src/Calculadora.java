dd7d115f669ddba2f423d4ea171ea93ba0adebde
Luis Fernando Cutilak <luiscutilak@gmail.com>
2024-04-09 13:24:53 -0300

    01- CALCULADORA -cria uma classe, com método que recebe um número como parametro e retorna o dobro

---
+++ src/Calculadora.java
@@ -0,0 +1,11 @@
+public class Calculadora {
+
+
+    public static void main(String[] args) {
+        System.out.println(recebeNumero(2));
+    }
+
+    private static double recebeNumero(double x) {
+        return x * 2;
+    }
+}
