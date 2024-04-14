d276fe21096364a1942fb2d5bf3ef4780c640526
Luis Fernando Cutilak <luiscutilak@gmail.com>
2024-04-14 17:04:12 -0300

    -Encapsulamento- Controlando o acesso e a escrita nos dados.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Main {
     public static void main(String[] args) {
 
         Filme meuFilme = new Filme();
-        meuFilme.nome = "Matrix";
-        meuFilme.anoDeLancamento = 199;
-        meuFilme.duracaoEmMinutos = 136;
-        meuFilme.incluidoNoPlano = true;
+        meuFilme.setNome("Matrix");
+        meuFilme.setAnoDeLancamento(1999);
+        meuFilme.setDuracaoEmMinutos(136);
+        meuFilme.setIncluidoNoPlano(true);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(10);
         meuFilme.avalia(10);
         meuFilme.avalia(8.7);
 
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
 
 
