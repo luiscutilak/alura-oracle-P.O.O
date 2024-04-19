ed74ca1c9342f6f5eae2b874e658d4d696fbc749
Luis Fernando Cutilak <luiscutilak@gmail.com>
2024-04-19 10:42:42 -0300

    Classificando episodios, filmes, series

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
@@ -44,6 +46,14 @@ public class Main {
         calculadora.inclui(lost);
         System.out.println("Tempo total: " + calculadora.getTempoTotal());
 
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(meuFilme);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
 
     }
 }
